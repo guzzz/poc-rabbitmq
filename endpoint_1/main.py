3be6d2765387715176c6e51a72a747254ebb3bda
Daniel Guzman <dguzman@id.uff.br>
2024-01-09 22:33:18 -0300

    change: alterando conteudo principal dos endpoints

--- endpoint_1/main.py
+++ endpoint_1/main.py
@@ -1,8 +1,17 @@
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from endpoint_1.services.processor import ProcessorService
+from endpoint_1.models import order
+from endpoint_1.config.database import engine
 
+
+order.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+process = ProcessorService()
 
 @app.get("/")
 async def root():
+    process.run()
     return {"message": "Hello World"}
