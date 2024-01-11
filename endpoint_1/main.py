556e26ce077c3b1f0c6488c9e9f3262ecf1690e9
Daniel Guzman <dguzman@id.uff.br>
2024-01-10 21:59:44 -0300

    add: insert data on db1

--- endpoint_1/main.py
+++ endpoint_1/main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI
+from sqlalchemy import text
 
 from endpoint_1.services.processor import ProcessorService
 from endpoint_1.models import order
@@ -7,6 +8,10 @@ from endpoint_1.config.database import engine
 
 
 order.Base.metadata.create_all(bind=engine)
+with engine.connect() as con:
+    with open("endpoint_1/config/load_base.sql") as file:
+        query = text(file.read())
+        con.execute(query)
 
 app = FastAPI()
 process = ProcessorService()
