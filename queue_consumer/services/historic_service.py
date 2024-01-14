cfb6a183019adfa6d44a445483e671f8659ca094
Daniel Guzman <dguzman@id.uff.br>
2024-01-13 22:33:28 -0300

    add: inserindo data de processamento

--- queue_consumer/services/historic_service.py
+++ queue_consumer/services/historic_service.py
@@ -1,14 +1,12 @@
 import uuid
 import json
 
-from structlog import get_logger
 from fastapi.encoders import jsonable_encoder
 from datetime import datetime
 
 from repositories.message_repository import MessageRepository
 
 message_repository = MessageRepository()
-log = get_logger()
 
 
 class HistoricService:
@@ -17,9 +15,8 @@ class HistoricService:
         self.database = message_repository
 
     def save(self, message_in_bytes):
-        log.info("[CREATE - message] Started creation message...")
         message_json = jsonable_encoder(message_in_bytes)
         message = json.loads(message_json)
-        message['created_at'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        message['processed_at'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
         # message["id"] = uuid.uuid4()
         return self.database.create(message)
