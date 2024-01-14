534f3c3068e99fe0c35312edc66f0c5a4a599393
Daniel Guzman <dguzman@id.uff.br>
2024-01-13 22:32:40 -0300

    refactor: processor

--- endpoint_1/services/processor.py
+++ endpoint_1/services/processor.py
@@ -1,16 +1,12 @@
 import os
-import json
-
-from datetime import datetime
 from structlog import get_logger
 from endpoint_1.services.redis import RedisService
-from endpoint_1.services.publisher import RabbitmqPublisher
+from endpoint_1.services.order import OrderService
 
 log = get_logger()
 redis = RedisService()
-rabbitmq_publisher = RabbitmqPublisher()
+order_service = OrderService()
 
-RUNNING_PROCESS = 'RUNNING'
 FINISHED_PROCESS = 'FINISHED'
 ERROR_PROCESS = 'ERROR'
 
@@ -20,78 +16,23 @@ class ProcessorService:
         self.__database = os.getenv("DATABASE_RESOURCE")
         self.__schema = os.getenv("SCHEMA_RESOURCE")
         self.__table = os.getenv("TABLE_RESOURCE")
-        self.__redis_key = os.getenv("REDIS_ENDPOINT_KEY")
-
-    def create_redis_payload(self, start_id, status):
-        payload = {
-            'database_resource': self.__database,
-            'schema_resource': self.__schema,
-            'table_resource': self.__table,
-            'start_date': str(datetime.now()),
-            'start_id': start_id,
-            'status_process': status
-        }
-        return json.dumps(payload)
-    
-    def create_queue_payload(self):
-        payload = {
-            'database_resource': self.__database,
-            'schema_resource': self.__schema,
-            'table_resource': self.__table,
-            'start_date': str(datetime.now()),
-        }
-        return json.dumps(payload) 
-    
-    def save_in_redis(self, start_id, status_process):
-        redis_payload = self.create_redis_payload(start_id, status_process)
-        redis.set_value(self.__redis_key, redis_payload)
 
     def start(self):
-        result = redis.get_value(self.__redis_key)
-        if result:            
-            if result.get("status_process", True) == 'FINISHED':
-                self.send_message(result.get('start_id', 0) + 1)
+        execution_info = redis.get_execution_info()
+        if execution_info:            
+            if execution_info.get("status_process") == ERROR_PROCESS:
+                log.error(f"[PROCESSOR] Execution Status blocked")
+                return
             else:
-                self.error_message(result.get("status_process"))
+                start_id = execution_info.get('last_id')
         else:
-            self.send_message(start_id=1)
-            
+            start_id = 0
+        self.process(start_id)
 
-    def run(self, start_id):
-        result = redis.get_value(self.__redis_key)
-        if result or start_id == 1:
-            return self.save_in_redis(start_id, RUNNING_PROCESS)
-        self.error_message(f"save: {RUNNING_PROCESS}")
-
-    def finish(self):
-        result = redis.get_value(self.__redis_key)
-        if result:
-            return self.save_in_redis(result.get('start_id'), FINISHED_PROCESS)
-        self.error_message(f"save: {FINISHED_PROCESS}")
-
-    def error(self):
-        result = redis.get_value(self.__redis_key)
-        if result:
-            return self.save_in_redis(result.get('start_id'), ERROR_PROCESS)    
-        self.error_message(f"save: {ERROR_PROCESS}")
-
-    def error_message(self, location=None):
-        log.error(f"[ERROR] Processor Service: {location}")
-
-    def send_message(self, start_id):
-        self.run(start_id)
-        queue_payload = self.create_queue_payload()
-        msg_sent_to_queue = self.send_to_queue(queue_payload)
-        log.info(f'Message sent to queue: {msg_sent_to_queue}')
-        if msg_sent_to_queue:
-            self.finish()
+    def process(self, start_id):
+        success, last_id = order_service.send_orders_to_queue(start_id)
+        if success:
+            log.info("[PROCESSOR] processed OK")
+            redis.save_execution_info(start_id, last_id, FINISHED_PROCESS)
         else:
-            self.error_message("Preparing to send_message")
-
-    def send_to_queue(self, payload):        
-        try:
-            rabbitmq_publisher.send_message(payload)
-            return True
-        except:
-            self.error_message("RabbitMQ send_to_queue")
-            return False
+            return redis.save_execution_info(start_id, last_id, ERROR_PROCESS)
